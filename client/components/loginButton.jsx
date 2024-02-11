d05457f63ac357bbcb6220462bf5f694bd4acb92
kylerasinger <kyle.rasinger@gmail.com>
2024-02-11 16:35:06 -0500

    Implemented session states and login to the header

--- client/components/loginButton.jsx
+++ client/components/loginButton.jsx
@@ -1,19 +1,32 @@
+import React from 'react';
+import { Inter } from "next/font/google";
+import Header from "./Header";
 import { useSession, signIn, signOut } from "next-auth/react"
 
-export default function Component() {
+const inter = Inter({ subsets: ["latin"] });
+
+export default function LoginButton() {
   const { data: session } = useSession()
   if (session) {
     return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
+        <>
+        <button 
+          className="text-white bg-black px-4 py-2 rounded hover:bg-gray-700" 
+          onClick={() => signOut()}
+        >
+          Sign out
+        </button>
       </>
     )
   }
   return (
     <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <button 
+        className="text-white bg-black px-4 py-2 rounded hover:bg-gray-700" 
+        onClick={() => signIn()}
+      >
+        Sign in
+      </button>
     </>
   )
 }
--- client/pages/loginTest.js
+++ client/pages/loginTest.js
@@ -1,4 +1,7 @@
-//pages boilerplate
+//
+//  This is a test page to test the login functionality of the app
+//  It was given by a tutorial and is not used in the app, used it to understand if you need 
+//
 import React from 'react';
 import { Inter } from "next/font/google";
 import Header from "../components/Header";
@@ -9,6 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Component() {
   const { data: session } = useSession()
+  console.log(session)
   if (session) {
     return (
       <>
