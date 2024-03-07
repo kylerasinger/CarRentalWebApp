56e8d1271122a3ea97c458dbff0cb0707089ac4a
kylerasinger <kyle.rasinger@gmail.com>
2024-03-07 13:34:12 -0500

    Create skeleton for admin console

--- client/components/adminConsole.jsx
+++ client/components/adminConsole.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Inter } from "next/font/google";
+import AdminOptionHeader from "../components/adminOptionHeader";
 
 export default function AdminConsole() {
     return (
         <div>
-            <h1>Admin Console</h1>
+            <AdminOptionHeader/>
         </div>
     )
 }
