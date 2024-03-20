92999f7b2d40d636ad93cd51db95bb12c0be32d4
Captaintom14 <60295341+Captaintom14@users.noreply.github.com>
2024-03-19 22:11:00 -0400

    Fixed Images Resolution
    Put the images in the public files and fixed the resolution of every image. Now, images look equally to each other.

--- client/components/CARS/CarData.jsx
+++ client/components/CARS/CarData.jsx
@@ -5,7 +5,7 @@ const car = [
       id: 1,
       name: 'Lamborghini',
       href: '#',
-      imageSrc : "/images/lamborghini.jpeg",
+      imageSrc : "/images/lambo.jpg",
       imageAlt: "Luxurious lamborghini",
       price: '$50',
    },
@@ -14,7 +14,7 @@ const car = [
     id: 2,
     name: 'Ford Mustang GT',
     href: '#',
-    imageSrc: 'https://hips.hearstapps.com/hmg-prod/images/img-2033-jpg-64bec5598090f.jpg?crop=0.752xw:1.00xh;0.129xw,0&resize=640:*',
+    imageSrc: "/images/ford.jpg",
     imageAlt: "2024 Ford Mustang GT",
     price: '$50'
    },
@@ -22,7 +22,7 @@ const car = [
   { id:3,
     name: 'Acura Integra Type S',
     href: '#',
-    imageSrc: 'https://tdrpmimages.azureedge.net/photos/import/202401/2701/0558/f221fc3f-86a2-42d5-97f5-d7e117477c31.jpg-1024x786',
+    imageSrc: "/images/acura.jpg",
     imageAlt: "2023 Acura Integra Type S",
     price: '$50'
   },
@@ -39,7 +39,7 @@ const car = [
   {id: 5,
    name: 'Honda Accord',
    href: '#',
-   imageSrc: "",
+   imageSrc: "/images/honda.png",
    imageAlt: "2021 Honda Accord",
    price: '$50'
   },
@@ -62,14 +62,14 @@ const car = [
   
   {id: 8,
    name: 'Porsche 718',
-   imageSrc: "" ,
+   imageSrc: "/images/porsche.jpeg",
    imageAlt: "2021 Porsche 718",
    price: '$50'
   },
   
   {id: 9,
     name: 'Porsche 718',
-    imageSrc: ,
+    imageSrc: "/images/porsche.jpeg" ,
     imageAlt: "2021 Porsche 718",
     price: '$50'
    }
