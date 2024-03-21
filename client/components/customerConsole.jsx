752a18b19afdf3f2c1d73c0147cb0e6140d2504f
khaled Elshokri <khaledelshokri28@outlook.com>
2024-03-21 08:15:50 -0400

    Added redirection to check-in

--- client/components/customerConsole.jsx
+++ client/components/customerConsole.jsx
@@ -1,14 +1,10 @@
 import CarDisplay from "../components/CARS/carDisplay";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
 
 export default function CustomerConsole() {
     return (
        
         <>
-            <Header />
             <CarDisplay />
-            <Footer />
         </>
     )
 }
