d139aad4d145c1351541e741f002feb19b617837
khaled Elshokri <khaledelshokri28@outlook.com>
2024-03-21 15:13:49 -0400

    clean up

--- client/components/Check-In/CheckInForm.jsx
+++ client/components/Check-In/CheckInForm.jsx
@@ -1,21 +1,10 @@
-import ReservationPage from '../RESERVATION/ReservationPage';
 import PaymentView from './PaymentView';
 
 const CheckInPage = () => {
-  // Mock data for selected car (replace with actual data)
-  //const selectedCar = {
-  //  name: 'Toyota Camry',
-  //  price: '$50/day',
-  //  description: 'A comfortable and reliable sedan.',
-  //  imageSrc: 'car-image-url.jpg',
-  //  imageAlt: 'Toyota Camry',
-  //};
-
   return (
 
 
     <>
-      <ReservationPage selectedCar />
       <PaymentView />
     </>
 
